178f706f5aa118496ee742be3041868296eae963
brandenlacour <brandenlacour@gmail.com>
2020-02-19 12:22:14 -0600

    can register user

--- src/AuthContainer/RegisterForm/index.js
+++ src/AuthContainer/RegisterForm/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Segment, Button } from "semantic-ui-react";
+import "./index.css";
 
 class RegisterForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       username: "",
       password: "",
@@ -16,57 +17,74 @@ class RegisterForm extends React.Component {
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.register(this.state);
+  };
   render() {
     return (
       <>
-        <Form size="small">
-          <Form.Field>
-            <label>Username</label>
-            <input
-              name="username"
-              value={this.state.username}
-              onChange={this.onChange}
-              placeholder="enter username"
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Password</label>
-            <input
-              name="password"
-              value={this.state.password}
-              onChange={this.onChange}
-              placeholder="enter password"
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Email</label>
-            <input
-              name="email"
-              value={this.state.email}
-              onChange={this.onChange}
-              placeholder="enter email"
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>City</label>
-            <input
-              name="city"
-              value={this.state.city}
-              onChange={this.onChange}
-              placeholder="enter city"
-            />
-          </Form.Field>
+        <div className="form-container">
+          <div>
+            <Form onSubmit={this.handleSubmit} className="form" size="small">
+              <Segment raised>
+                <Form.Field>
+                  <span className="title">
+                    <h3>Register</h3>
+                  </span>
+                  <label>Username</label>
+                  <input
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChange}
+                    placeholder="enter username"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <input
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    placeholder="enter password"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Email</label>
+                  <input
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    placeholder="enter email"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>City</label>
+                  <input
+                    name="city"
+                    value={this.state.city}
+                    onChange={this.onChange}
+                    placeholder="enter city"
+                  />
+                </Form.Field>
 
-          <Form.Field>
-            <label>Picture</label>
-            <input
-              name="picture"
-              value={this.state.picture}
-              onChange={this.onChange}
-              placeholder="enter picture url"
-            />
-          </Form.Field>
-        </Form>
+                <Form.Field>
+                  <label>Picture</label>
+                  <input
+                    name="picture"
+                    value={this.state.picture}
+                    onChange={this.onChange}
+                    placeholder="enter picture url"
+                  />
+                </Form.Field>
+                <div className="submit">
+                  <Button>Submit</Button>
+                </div>
+              </Segment>
+            </Form>
+          </div>
+        </div>
       </>
     );
   }
