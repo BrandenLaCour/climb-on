34fb0879fdb461dfae2f03442929806d1c068a01
brandenlacour <brandenlacour@gmail.com>
2020-02-20 10:37:47 -0600

    fixed login bug

--- src/App.js
+++ src/App.js
@@ -16,10 +16,11 @@ class App extends Component {
     super();
     this.state = {
       loggedIn: false,
-      username: "brandenlacour@gmail.com",
+      username: "",
       page: "home",
       climbToBeEdited: -1,
-      climb: null
+      climb: null,
+      climbs: []
     };
   }
   // {
@@ -29,80 +30,111 @@ class App extends Component {
   //   "city": "chicago"
   // }
 
+  async componentDidMount() {
+    const climbsResponse = await fetch(
+      process.env.REACT_APP_API_URI + "/api/v1/climbs/all"
+    );
+    const { data } = await climbsResponse.json();
+    const reversed = data.reverse();
+    //reverse to have most recent climbs show up
+    this.setState({ climbs: data });
+  }
+
   handlePageChoice = page => {
     this.setState({ page: page });
   };
 
   register = async userInfo => {
-    const registerResponse = await fetch(
-      process.env.REACT_APP_API_URI + "/api/v1/users/register",
-      {
-        credentials: "include",
-        method: "POST",
-        body: JSON.stringify(userInfo),
-        headers: {
-          "content-type": "application/json"
+    try {
+      const registerResponse = await fetch(
+        process.env.REACT_APP_API_URI + "/api/v1/users/register",
+        {
+          credentials: "include",
+          method: "POST",
+          body: JSON.stringify(userInfo),
+          headers: {
+            "content-type": "application/json"
+          }
         }
-      }
-    );
-    const { data } = await registerResponse.json();
-    this.setState({ username: data.username });
+      );
+      const { data } = await registerResponse.json();
+      this.setState({ username: data.username });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   login = async userInfo => {
-    const loginResponse = await fetch(
-      process.env.REACT_APP_API_URI + "/api/v1/users/login",
-      {
-        credentials: "include",
-        method: "POST",
-        body: JSON.stringify(userInfo),
-        headers: {
-          "content-type": "application/json"
+    try {
+      const loginResponse = await fetch(
+        process.env.REACT_APP_API_URI + "/api/v1/users/login",
+        {
+          credentials: "include",
+          method: "POST",
+          body: JSON.stringify(userInfo),
+          headers: {
+            "content-type": "application/json"
+          }
         }
+      );
+      const loginJson = await loginResponse.json();
+      if (loginJson.message !== "username or password incorrect") {
+        this.setState({ username: loginJson.data.username });
       }
-    );
-    const { data } = await loginResponse.json();
-
-    this.setState({ username: data.username });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   logout = async () => {
-    const logoutResponse = await fetch(
-      process.env.REACT_APP_API_URI + "/api/v1/users/logout"
-    );
-    const logoutJson = await logoutResponse.json();
-    console.log(logoutJson);
-    this.setState({ username: "" });
+    try {
+      const logoutResponse = await fetch(
+        process.env.REACT_APP_API_URI + "/api/v1/users/logout"
+      );
+      const logoutJson = await logoutResponse.json();
+      console.log(logoutJson);
+      this.setState({ username: "" });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   createClimb = async climbInfo => {
-    const createdResponse = await fetch(
-      process.env.REACT_APP_API_URI + "/api/v1/climbs/",
-      {
-        credentials: "include",
-        method: "POST",
-        body: JSON.stringify(climbInfo),
-        headers: {
-          "content-type": "application/json"
+    try {
+      const createdResponse = await fetch(
+        process.env.REACT_APP_API_URI + "/api/v1/climbs/",
+        {
+          credentials: "include",
+          method: "POST",
+          body: JSON.stringify(climbInfo),
+          headers: {
+            "content-type": "application/json"
+          }
         }
-      }
-    );
-    const createdJson = await createdResponse.json();
+      );
+      const createdJson = await createdResponse.json();
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   updateClimb = async climbInfo => {
-    const updatedResponse = await fetch(
-      process.env.REACT_APP_API_URI + "/api/v1/climbs/",
-      {
-        credentials: "include",
-        method: "PUT",
-        body: JSON.stringify(climbInfo),
-        headers: {
-          "content-type": "application/json"
+    try {
+      const updatedResponse = await fetch(
+        process.env.REACT_APP_API_URI + "/api/v1/climbs/" + climbInfo.id,
+        {
+          credentials: "include",
+          method: "PUT",
+          body: JSON.stringify(climbInfo),
+          headers: {
+            "content-type": "application/json"
+          }
         }
-      }
-    );
-    const updatedJson = await updatedResponse.json();
+      );
+      const updatedJson = await updatedResponse.json();
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   showClimb = async climbInfo => {
@@ -180,14 +212,24 @@ class App extends Component {
                   <Route
                     exact
                     path="/"
-                    render={props => (
-                      <ClimbsContainer
-                        editClimb={this.editClimb}
-                        showClimb={this.showClimb}
-                        username={this.state.username}
-                        page={this.state.page}
-                      />
-                    )}
+                    render={props =>
+                      this.state.climbs.length > 0 ? (
+                        <ClimbsContainer
+                          climbs={this.state.climbs}
+                          editClimb={this.editClimb}
+                          showClimb={this.showClimb}
+                          username={this.state.username}
+                          page={this.state.page}
+                        />
+                      ) : (
+                        <AuthForm
+                          handlePageChoice={this.handlePageChoice}
+                          formType={this.state.page}
+                          register={this.register}
+                          login={this.login}
+                        />
+                      )
+                    }
                   />
                   <Route
                     exact
--- src/ClimbsContainer/index.js
+++ src/ClimbsContainer/index.js
@@ -1,34 +1,15 @@
 import React from "react";
 import ClimbList from "./ClimbsList";
 
-class ClimbsContainer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      climbs: []
-    };
-  }
-
-  async componentDidMount() {
-    const climbsResponse = await fetch(
-      process.env.REACT_APP_API_URI + "/api/v1/climbs/all"
-    );
-    const { data } = await climbsResponse.json();
-    const reversed = data.reverse();
-    //reverse to have most recent climbs show up
-    this.setState({ climbs: data });
-  }
-
-  render() {
-    return (
-      <ClimbList
-        editClimb={this.props.editClimb}
-        showClimb={this.props.showClimb}
-        username={this.props.username}
-        climbs={this.state.climbs}
-      />
-    );
-  }
-}
+const ClimbsContainer = props => {
+  return (
+    <ClimbList
+      editClimb={this.props.editClimb}
+      showClimb={this.props.showClimb}
+      username={this.props.username}
+      climbs={props.climbs}
+    />
+  );
+};
 
 export default ClimbsContainer;
