f5a8f6ee63ddfb13ac84b207b708a253248c0a60
brandenlacour <brandenlacour@gmail.com>
2020-02-20 14:05:10 -0600

    users cards styling

--- src/UsersContainer/UserCard/index.js
+++ src/UsersContainer/UserCard/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const UserCard = props => {
   const climbs = props.climbs.filter(climb => climb.id === props.user.id);
   return (
     <div>
-      <Card>
+      <Card style={{ width: "250px" }}>
         <Card.Content>
           <Image floated="right" size="mini" src={props.user.picture} />
           <Card.Header>{props.user.username}</Card.Header>
           <Card.Meta>{props.user.city}</Card.Meta>
-          <Card.Description>climbs posted: {climbs.length}</Card.Description>
+          <Card.Description>
+            climbs posted: {climbs.length}{" "}
+            <Link style={{ marginLeft: "40px" }}>More Info</Link>
+          </Card.Description>
         </Card.Content>
       </Card>
     </div>
